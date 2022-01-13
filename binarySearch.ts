1bcb8647f2b14805ca8adf0308328d69e5c77176
shpigluto <guysp@soluto.com>
2022-01-13 02:04:17 +0200

    binary search altorithm created

--- binarySearch.ts
+++ binarySearch.ts
@@ -1,2 +1,20 @@
-const message: string = 'hi'
-console.log(message)
+type Index = number;
+const binarySearch = (sortedArray:number[], item: number): Index => {
+    let bottom: Index = 0;
+    let top: Index = sortedArray.length -1;
+
+    while(bottom <= top ){
+        let guess : Index = Math.floor( (bottom + top) / 2);
+        const currentItem = sortedArray[guess]
+        if(currentItem === item) return guess
+        if(item < currentItem){
+            top = guess -1
+        } else{
+            bottom = guess + 1
+        }
+    }
+    return -1
+}
+
+const nums = [1,2,6,33,56,97,203]
+console.log(binarySearch(nums, 97))
