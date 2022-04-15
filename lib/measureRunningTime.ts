12f9d3d92ce619e59518d007032b991a7960e7bf
shpiglify <shpiglify@gmail.com>
2022-04-15 12:41:22 +0300

    refactor: fix ts errors

--- lib/measureRunningTime.ts
+++ lib/measureRunningTime.ts
@@ -1,6 +1,6 @@
 
-export default function measureRunningTime(func,...args){
-    const varToString = varObj => Object.keys(varObj)[0]
+export default function measureRunningTime(func: (...args: any[]) => any, ...args:any){
+    const varToString:(varObj:any)=>string = varObj => Object.keys(varObj)[0]
     const displayName = func.name || varToString({ func })
     console.time(displayName)
     func(...args)
